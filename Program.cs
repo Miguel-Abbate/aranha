6e6bf9a258a408d860b442d1155fe035fa8f1266
Miguel-Abbate <abbatejr@gmail.com>
2025-09-01 13:20:54 -0300

    4° versão

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
 ﻿for (int i = 0; i < 5; i++)
 {
     Console.Clear();
-    Console.Beep(1485,500);
-    Console.ForegroundColor = ConsoleColor.Red;
+    Console.Beep(1485, 500);
+    Console.ForegroundColor = ConsoleColor.Blue;
 
     Console.WriteLine(" ||    ||");
     Console.WriteLine(" \\\\(  )// ");
@@ -12,7 +12,7 @@
     Console.WriteLine(" ||    ||");
     Thread.Sleep(500);
     Console.Clear();
-    Console.Beep(1650,500);
+    Console.Beep(1485, 500);
     Console.ForegroundColor = ConsoleColor.Red;
     Console.WriteLine("   ||    ||");
     Console.WriteLine("   \\\\(  )// ");
@@ -22,8 +22,8 @@
     Console.WriteLine("   ||    ||");
     Thread.Sleep(500);
     Console.Clear();
-    Console.Beep(2200,500);
-    Console.ForegroundColor = ConsoleColor.Red;
+    Console.Beep(2200, 500);
+    Console.ForegroundColor = ConsoleColor.Green;
     Console.WriteLine("     ||    ||");
     Console.WriteLine("     \\\\(  )// ");
     Console.WriteLine("      (    )");
@@ -32,8 +32,9 @@
     Console.WriteLine("     ||    ||");
     Thread.Sleep(500);
     Console.Clear();
-    Console.Beep(1759,500);
-    Console.ForegroundColor = ConsoleColor.Red;
+
+    Console.Beep(1485, 500);
+    Console.ForegroundColor = ConsoleColor.Magenta;
     Console.WriteLine("       ||    ||");
     Console.WriteLine("       \\\\(  )// ");
     Console.WriteLine("        (    )");
@@ -42,8 +43,8 @@
     Console.WriteLine("       ||    ||");
     Thread.Sleep(500);
     Console.Clear();
-    Console.Beep(1980,500);
-    Console.ForegroundColor = ConsoleColor.Red;
+    Console.Beep(1485, 500);
+    Console.ForegroundColor = ConsoleColor.Cyan;
     Console.WriteLine("         ||    ||");
     Console.WriteLine("         \\\\(  )// ");
     Console.WriteLine("          (    )");
@@ -52,8 +53,8 @@
     Console.WriteLine("         ||    ||");
     Thread.Sleep(500);
     Console.Clear();
-    Console.Beep(2200,500);
-    Console.ForegroundColor = ConsoleColor.Red;
+    Console.Beep(2200, 500);
+    Console.ForegroundColor = ConsoleColor.Gray;
     Console.WriteLine("           ||    ||");
     Console.WriteLine("           \\\\(  )// ");
     Console.WriteLine("            (    )");
@@ -62,8 +63,8 @@
     Console.WriteLine("           ||    ||");
     Thread.Sleep(500);
     Console.Clear();
-    Console.Beep(2200,500);
-    Console.ForegroundColor = ConsoleColor.Red;
+    Console.Beep(2200, 500);
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
     Console.WriteLine("             ||    ||");
     Console.WriteLine("             \\\\(  )// ");
     Console.WriteLine("              (    )");
@@ -72,8 +73,15 @@
     Console.WriteLine("             ||    ||");
     Thread.Sleep(500);
     Console.Clear();
+
+
 }
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+Console.WriteLine(" ");
+Console.WriteLine(" ");
 Console.WriteLine(" Obrigado pela Ateção!!!");
+Console.WriteLine(" ");
+Console.WriteLine(" ");
 Console.ResetColor();
 
 
